1573a3e4ada353fd0d5f403b72f2292d1e128101
Yuchen Zhang <1767230250@qq.com>
2023-10-31 19:52:25 +0800

    append-only流demo

--- src/main/java/com/bw/flinksql/Job3.java
+++ src/main/java/com/bw/flinksql/Job3.java
@@ -16,26 +16,31 @@ public class Job3 {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         StreamTableEnvironment streamTableEnvironment = StreamTableEnvironment.create(env);
         //创建一条datastream
-        DataStreamSource<Tuple2<String, String>> tuple2DataStreamSource = env.fromElements(
-                Tuple2.of("zhangsan", "84L"),
-                Tuple2.of("lisi", "95L"),
-                Tuple2.of("zhangsan", "99L"),
-                Tuple2.of("lisi", "73L"),
-                Tuple2.of("zhangsan", "66L")
-                );
-        //根据这个DataStream创建一张表
-        Table table = streamTableEnvironment.fromDataStream(tuple2DataStreamSource);
+        DataStreamSource<Row> rowDataStreamSource = env.fromElements(
+                Row.of("zhangsan", 95),
+                Row.of("lisi", 77),
+                Row.of("wangwu", 82),
+                Row.of("zhangsan", 60),
+                Row.of("lisi", 88),
+                Row.of("zhangsan", 95),
+                Row.of("zhaoliu", 57)
+        );
+
+        //依据datastream生成table
+        Table table = streamTableEnvironment.fromDataStream(rowDataStreamSource).as("name", "score");
+
+        //创建临时视图
         streamTableEnvironment.createTemporaryView("table1", table);
 
-        //执行SQL
-        Table table1 = streamTableEnvironment.sqlQuery("select * from table1");
+        //利用sql聚合函数计算成绩平均值
+        Table result = streamTableEnvironment.sqlQuery("select name, avg(score) from table1 group by name");
 
-        //再将其转换为一个datastream
-        DataStream<Row> rowDataStream = streamTableEnvironment.toDataStream(table1);
+        //转换为stream
+        DataStream<Row> rowDataStream = streamTableEnvironment.toChangelogStream(result);
 
         rowDataStream.print();
 
-        env.execute("job2");
+        env.execute("job3");
 
     }
 }
