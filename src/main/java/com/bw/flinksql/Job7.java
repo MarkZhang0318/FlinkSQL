1e7adabc08e62833473fb32cfc4109d32750f896
ZhangYuchen <1767230250@qq.com>
2023-11-02 16:37:35 +0800

    Job7

---
+++ src/main/java/com/bw/flinksql/Job7.java
@@ -0,0 +1,21 @@
+package com.bw.flinksql;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Schema;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableDescriptor;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+/*
+* TableDescriptor
+* */
+public class Job7 {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        StreamTableEnvironment tableEnvironment = StreamTableEnvironment.create(env);
+
+
+
+    }
+}
